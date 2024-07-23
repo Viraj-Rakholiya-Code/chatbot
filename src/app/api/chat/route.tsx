b2290cf7420531a7e24d12e41b5ca622acb9703c
Viraj Rakholiya <virajrakholiya14@gmail.com>
2024-07-23 16:27:35 +0530

    commit

---
+++ src/app/api/chat/route.tsx
@@ -0,0 +1,30 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from "ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
+
+const buildGoogleGenAIPrompt = (messages: Message[]) => ({
+  contents: messages
+    .filter(
+      (message) => message.role === "user" || message.role === "assistant"
+    )
+    .map((message) => ({
+      role: message.role === "user" ? "user" : "model",
+      parts: [
+        { text: message.content },
+        { text: "your name is viraj rakholiya" },
+      ],
+    })),
+});
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const geminiStream = await genAI
+    .getGenerativeModel({ model: "gemini-pro" })
+    .generateContentStream(buildGoogleGenAIPrompt(messages));
+
+  const stream = GoogleGenerativeAIStream(geminiStream);
+
+  return new StreamingTextResponse(stream);
+}
